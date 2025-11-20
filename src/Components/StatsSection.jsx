474ca2ff4377184c01121fda466fc8cd5f9d7622
AKMZakaria <akmzakaria488@gmail.com>
2025-11-20 14:56:54 +0600

    done

--- src/Components/StatsSection.jsx
+++ src/Components/StatsSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const StatsSection = () => {
   return (
@@ -25,7 +25,7 @@ const StatsSection = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default StatsSection;
+export default StatsSection
